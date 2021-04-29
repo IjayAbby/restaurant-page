aa383e86eca1612cda868e7612882fc11e2371db
Ijay Abby <inyangasi4@gmail.com>
2021-04-29 15:36:08 +0300

    Fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    mode:"production",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    },
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
